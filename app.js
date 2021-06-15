7e5a6a6260fa3aa2fc74150291934bc0f5dcea08
Nadeem Beg <nadeem.beg@ifuturz.com>
2021-06-15 19:53:16 +0530

    ckiclup1

--- app.js
+++ app.js
@@ -111,4 +111,6 @@ app.get('/login', function(req, res) {
 
 
 const port = process.env.PORT || 3000;
-app.listen(port , () => console.log('App listening on port ' + port));
+app.listen(process.env.PORT || 3000 , () => {
+  console.log('App listening on port ' + port)
+});
